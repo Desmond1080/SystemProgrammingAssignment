9588514d9daeed9eeff3f3551428a0a29a6d6364
eepyxt <xiangting0221@gmail.com>
2025-09-06 18:21:00 +0800

    Event Manager update

--- Event.h
+++ Event.h
@@ -1,65 +1,63 @@
-#ifndef EVENT_H
+﻿#ifndef EVENT_H
 #define EVENT_H
 
-#include <iostream>
 #include <string>
 #include <vector>
 #include <utility>
 #include <ctime>
+#include <iostream>
 
 using namespace std;
 
 enum class EventCategory {
-    Corporate,
-    TradeShow,
-    Fundraising,
-    Personal,
-    Award,
-    Festival,
-    Wedding,
-    Seminar
+    Concert,
+	Sports,
+    Workshop,
+    Music,
+    Marathon,
+    Charity,
+    Comedy,
+    Theatre,
+    Pet,
+    Food,
+	Technology,
+	Competition,
+    Festival
 };
 
-// Convert enum to string
-string categoryToString(EventCategory category);
-
-struct Event {
+class Event {
+public:
     string name;
     string description;
     EventCategory category;
-    time_t timestamp{};    // convert to string later
+    time_t date;
     string location;
     string organizer;
-    double ticketPrice{};
+    double ticketPrice;
 
-    // Ticket categories: { "VIP", {price, available} }
-    vector<pair<string,pair<double, int>>> categoryOptions;
+    // Ticket categories: {name, {price, available}}
+    vector<pair<string, pair<double, int>>> categoryOptions;
 
-    Event(const string& name = "",
-          const string& description = "",
-          EventCategory category = EventCategory::Corporate,
-          time_t timestamp = 0,
-          const string& location = "",
-          const string& organizer = "",
-          double ticketPrice = 0.0,
-          const vector<pair<string, pair<double, int>>>& categoryOptions = {});
+    Event(const string& name,
+        const string& description,
+        EventCategory category,
+        time_t date,
+        const string& location,
+        const string& organizer,
+        double ticketPrice,
+        const vector<pair<string, pair<double, int>>>& categoryOptions);
+
+    Event() = default;
 
     void addCategory(const string& catName, double price, int available);
     void printDetails() const;
-};
 
-class EventManager {
-private:
-    vector<Event> events;
+    void setDate(time_t newDate);
+    time_t getDate() const;
 
-public:
-    void addEvent(const Event& event);
-    void listEvents() const;
-    vector<Event> getEvents() const;
-    void editEvent(size_t index, const Event& updatedEvent);
-    void deleteEvent(size_t index);
-    vector<Event> searchEventsByName(const std::string& name) const;
-    vector<Event> searchEventsByCategory(EventCategory category) const;
+    void updateCategoryCapacity(size_t index, int newCapacity);
+    void listCategories() const;
+
 };
 
 #endif
